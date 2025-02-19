7b13a904e3e78ba125908d0a36bbd7b85d123d39
Nalfeinn <nalfeinn@Linux-nalfeinn-huawei.nalfeinn-domain>
2025-02-19 09:13:53 +0100

    try to correct colleen, not finished

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -1,18 +1,17 @@
 ;colleen exercise
+section .data
+code db ";colleen exercise%1$csection .data%1$c   code db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal main%1$cextern printf%1$c%1$cmain:%1$c  lea rdi, [code]%1$c  mov rsi, 0xa%1$c  mov rdx, 0x22%1$c  lea rcx, [code]%1$c  call printf%1$c  mov rax, 60%1$c  xor rdi, rdi%1$c  syscall ;inside main com", 0
+
+section .text
 global main
 extern printf
 
-section .data
-code db ";colleen exercise%1$cglobal main%1$cextern printf%1$c%1$csection .data%1$ccode db %3$c%2$s%3$c, 0x0%1$c%1$csection .text%1$c%1$cmain:%1$c  push rbx%1$c  mov rdi, code%1$c  mov rsi, 0xa%1$c  mov rcx, 0x22%1$c  mov rdx, code%1$c  call printf%1$c  pop rsi%1$c  ret ;inside main com", 0x0
-
-section .text
-
 main:
-  push rbx
-  mov rdi, code
+  lea rdi, [code]
   mov rsi, 0xa
-  mov rcx, 0x22
-  mov rdx, code
+  mov rdx, 0x22
+  lea rcx, [code]
   call printf
-  pop rsi
-  ret ;inside main com
+  mov rax, 60
+  xor rdi, rdi
+  syscall ;inside main com
--- asm/Sully.s
+++ asm/Sully.s
@@ -4,10 +4,11 @@ section .data
   testFilename db "Sully_5.s", 0
   format db "section .data%1$c  filename db %2$cSully_5.s%2$c, 0%1$c  testFilename db %2$cSully_5.s%2$c, 0%1$c  format db %2$c%3$s%2$c, 0%1$c  i dq %4$d%1$c%1$csection .text%1$c  extern dprintf%1$c  global main%1$c%1$cmain:%1$c  mov rax, 2%1$c  lea rdi, [testFilename]%1$c  mov rsi, 0%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  js  create_file%1$c  dec qword [i]%1$c%1$c  mov rax, 3%1$c  syscall%1$c%1$ccreate_file:%1$c  lea rdi, [filename]%1$c  mov rsi, i%1$c  call dprintf%1$c  mov [filename], rax%1$c%1$c  mov rax, 2%1$c  lea rdi, [filename]%1$c  mov rsi, 0x241%1$c  mov rdx, 0x1b6%1$c  syscall%1$c  test rax, rax%1$c  jz exit%1$c  mov rdi, rax%1$c%1$c  lea rsi, [format]%1$c  mov rdx, 0xa%1$c  mov rcx, 0x22%1$c  mov r8, [format]%1$c  mov r9, [i]%1$c  mov rax, 0x25%1$c  push rax%1$c  call dprintf%1$c%1$c  mov rax, 3%1$c  syscall%1$c%1$cexit:%1$c  mov rax, 60%1$c  xor rdi, rdi%1$c  syscall", 0
   i dq 5
-  debug db "debug"
+  debug db "debug", 0
+  debug_len equ $ - debug  ; Calculate message length
 
 section .text
-  extern dprintf, sprintf
+  extern dprintf, sprintf, printf
   global main
 
 main:
@@ -38,7 +39,6 @@ create_file:
   jz exit
   mov rdi, rax
 
-  ;push 0x25
   lea rsi, [format]
   mov rdx, 0xa
   mov rcx, 0x22
